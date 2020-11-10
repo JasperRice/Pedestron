6a231bc597b596ecd401f465478c8077b8fd9848
Sifan Jiang <jasper.sifan@gmail.com>
2020-11-10 11:45:04 +0800

    Update demo.sh

--- shell/demo.sh
+++ shell/demo.sh
@@ -1,1 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 python tools/demo.py configs/elephant/cityperson/cascade_hrnet.py ./models_pretrained/epoch_5.pth.stu demo/ result_demo/ --image_type 'png'
+CUDA_VISIBLE_DEVICES=1 \
+    python ../tools/demo.py \
+    ../configs/elephant/cityperson/cascade_hrnet.py \
+    /data/sifan/model-zoo/pedestron/cascade_mask_rcnn_citypersons_hrnet_epoch_5.pth.stu \
+    ../demo/ \
+    ../result_demo/ \
+    --image_type 'png'
