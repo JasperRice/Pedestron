d2e25618c96e3629cda480936efcc3e84a9491e7
Sifan Jiang <jasper.sifan@gmail.com>
2020-11-11 17:30:59 +0800

    Add a script for plotting height vs distance

--- tools/plot_height_vs_distance.py
+++ tools/plot_height_vs_distance.py
@@ -1,4 +1,13 @@
 from matplotlib import pyplot as plt
 
-with open('height-distance-webcam.txt') as file:
-    pass
+heights = []
+distances = []
+with open('height-distance-webcam.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        height, distacne = tuple(map(int, line.split()))
+        heights.append(height)
+        distances.append(distacne)
+
+plt.scatter(heights, [1/distacne for distacne in distances])
+plt.savefig("TEMP.jpg")
