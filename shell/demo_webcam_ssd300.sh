29f9776bcbef9f10acaa6bb0804028ead8147e4b
Sifan <jiangsifan@qq.com>
2020-11-14 10:48:15 +0800

    Add two shell scripts to run different detector for webcam

---
+++ shell/demo_webcam_ssd300.sh
@@ -0,0 +1,5 @@
+# Usage: sh shell/webcam.sh
+CUDA_VISIBLE_DEVICES=0 \
+    python tools/demo.py \
+    configs/pascal_voc/ssd300_voc.py \
+    /home/midea/Documents/model_zoo/pedestron/ssd300_voc_vgg16_caffe_240e_20190501-7160d09a.pth
