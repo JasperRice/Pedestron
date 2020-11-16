060acb109c6b095c87f2fc25a02a8edd57f0d1a0
Sifan Jiang <jasper.sifan@gmail.com>
2020-11-16 16:35:27 +0800

    Update

--- shell/demo_webcam_ssd300.sh
+++ shell/demo_webcam_ssd300.sh
@@ -2,4 +2,5 @@
 CUDA_VISIBLE_DEVICES=0 \
     python tools/demo.py \
     configs/pascal_voc/ssd300_voc.py \
-    /home/midea/Documents/model_zoo/pedestron/ssd300_voc_vgg16_caffe_240e_20190501-7160d09a.pth
+    /home/midea/Documents/model_zoo/pedestron/ssd300_voc_vgg16_caffe_240e_20190501-7160d09a.pth \
+    --save_bbox False
