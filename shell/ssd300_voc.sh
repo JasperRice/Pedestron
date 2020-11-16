1fe5f3803fbff12711c7f65b1c8fd0370bb652c0
Sifan Jiang <jasper.sifan@gmail.com>
2020-11-16 09:54:12 +0800

    Try to add a config file to enable fp16 on ssd300

--- shell/ssd300_voc.sh
+++ shell/ssd300_voc.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=6 \
     python tools/demo.py \
     configs/pascal_voc/ssd300_voc.py \
     /data/sifan/model-zoo/pedestron/ssd300_voc_vgg16_caffe_240e_20190501-7160d09a.pth \
-    /data/sifan/images/original/follow/webcam/ \
-    /data/sifan/images/results/follow/webcam/ \
+    --input_img_dir /data/sifan/images/original/follow/webcam/ \
+    --output_dir /data/sifan/images/results/follow/webcam/ \
     --image_type jpg \
     --save_bbox False
