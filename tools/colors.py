7e53a3105429c57d9cc99f15ebd662066684cb0f
Sifan <jiangsifan@qq.com>
2020-11-19 18:13:16 +0800

    Update

---
+++ tools/colors.py
@@ -0,0 +1,29 @@
+# BGR
+BGR = {
+    'Black'                 : (0,0,0),
+    'White'                 : (255,255,255),
+
+    'Blue'                  : (255,0,0),
+    'Cyan'                  : (255,255,0),
+    'Green'                 : (0,255,0),
+    'Yellow'                : (0,255,255),
+    'Red'                   : (0,0,255),
+    'Magenta'               : (255,0,255),
+
+    'MidnightBlue'          : (112,25,25),
+    'NavyBlue'              : (128,0,0),
+    'CornflowerBlue'        : (237,149,100),
+    'DarkSlateBlue'         : (139,61,72),
+    'SlateBlue'             : (205,90,106),
+    'MediumSlateBlue'       : (238,104,123),
+    'LightSlateBlue'        : (255,112,132),
+
+    'DarkSeaGreen1'         : (193,255,193),
+    'DarkSeaGreen2'         : (180,238,180),
+    'DarkSeaGreen3'         : (155,205,155),
+    'SeaGreen1'             : (159,255,84),
+    'SeaGreen2'             : (148,238,78),
+
+    'Firebrick1'            : (48,48,255),
+    'Firebrick2'            : (44,44,238),
+}
