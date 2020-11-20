95ef2213456b95fac47d360d8f52fe0b1b66d26a
Sifan <jiangsifan@qq.com>
2020-11-20 08:55:19 +0800

    Add test in IoU.py

--- tools/IoU.py
+++ tools/IoU.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def cal_iou(bboxes, target):
     """Calculate the IoU between bboxes and the target
 
@@ -12,16 +13,27 @@ def cal_iou(bboxes, target):
     """
     bboxes = np.atleast_2d(bboxes)
 
-    area_bboxes = (bboxes[:,2] - bboxes[:,0]) * (bboxes[:,3] - bboxes[:,1])
+    area_bboxes = (bboxes[:, 2] - bboxes[:, 0]) * (bboxes[:, 3] - bboxes[:, 1])
     area_target = (target[2] - target[0]) * (target[3] - target[1])
 
-    iou_x1 = np.maximum(bboxes[:,0], target[0])
-    iou_y1 = np.maximum(bboxes[:,1], target[1])
-    iou_x2 = np.minimum(bboxes[:,2], target[2])
-    iou_y2 = np.minimum(bboxes[:,3], target[3])
+    iou_x1 = np.maximum(bboxes[:, 0], target[0])
+    iou_y1 = np.maximum(bboxes[:, 1], target[1])
+    iou_x2 = np.minimum(bboxes[:, 2], target[2])
+    iou_y2 = np.minimum(bboxes[:, 3], target[3])
 
     iou_w = iou_x2 - iou_x1
     iou_h = iou_y2 - iou_y1
     area_iou = iou_w * iou_h
-    
+
     return area_iou / (area_bboxes + area_target - area_iou)
+
+
+if __name__ == "__main__":
+    bboxes = np.array([[1, 2, 3, 4],
+                       [2, 3, 4, 5]])
+    target = np.array([1,2,3,4])
+
+    ious = cal_iou(bboxes, target)
+    print(ious)
+    print(ious.shape)
+    print(type(ious))
