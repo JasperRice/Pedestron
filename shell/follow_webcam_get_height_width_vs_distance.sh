1ac15b5fc18dda1da2af403b8786fa7093c24603
Sifan Jiang <jasper.sifan@gmail.com>
2020-11-25 09:30:08 +0800

    Add an script to get new height and width vs distance

---
+++ shell/follow_webcam_get_height_width_vs_distance.sh
@@ -0,0 +1,9 @@
+# Usage: sh shell/follow_webcam_get_height_width_vs_distance.sh
+CUDA_VISIBLE_DEVICES=6 \
+    python tools/demo.py \
+    configs/elephant/cityperson/cascade_hrnet.py \
+    /cv_data/sifan/model-zoo/pedestron/cascade_mask_rcnn_citypersons_hrnet_epoch_5.pth.stu \
+    /cv_data/sifan/images/original/follow/distance/dw800/ \
+    /cv_data/sifan/images/results/follow/distance/dw800/ \
+    --image_type jpg \
+    --save_bbox True
