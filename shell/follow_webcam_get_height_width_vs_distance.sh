6f09dbfa5d4fb27df04e1a069500c18b36489285
Sifan Jiang <jasper.sifan@gmail.com>
2020-11-25 09:31:08 +0800

    Fix shell argument

--- shell/follow_webcam_get_height_width_vs_distance.sh
+++ shell/follow_webcam_get_height_width_vs_distance.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=6 \
     python tools/demo.py \
     configs/elephant/cityperson/cascade_hrnet.py \
     /cv_data/sifan/model-zoo/pedestron/cascade_mask_rcnn_citypersons_hrnet_epoch_5.pth.stu \
-    /cv_data/sifan/images/original/follow/distance/dw800/ \
-    /cv_data/sifan/images/results/follow/distance/dw800/ \
+    --input_img_dir /cv_data/sifan/images/original/follow/distance/dw800/ \
+    --output_dir /cv_data/sifan/images/results/follow/distance/dw800/ \
     --image_type jpg \
     --save_bbox True
