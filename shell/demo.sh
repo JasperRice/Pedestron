102298921cd64aae9a8afddb1563e6b077d1e090
JasperRice <jiangsifan@qq.com>
2020-11-26 10:07:47 +0800

    Update

--- shell/demo.sh
+++ shell/demo.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 \
     python tools/demo.py \
     configs/elephant/cityperson/cascade_hrnet.py \
-    /home/midea/Documents/model_zoo/pedestron/cascade_mask_rcnn_citypersons_hrnet_epoch_5.pth.stu \
+    models/cascade_mask_rcnn_citypersons_hrnet_epoch_5.pth.stu \
     --input_img_dir /home/midea/Documents/Pedestron/demo \
     --output_dir /home/midea/Documents/Pedestron/result_demo \
     --image_type png
